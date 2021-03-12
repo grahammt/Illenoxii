b8ba349bcb928530e9d18a5f1117cba6ba26922d
wlshen123 <wesleylshen@gmail.com>
2021-03-12 03:26:32 -0500

    Added foundation to lab_wesley scene

---
+++ BestEffortGamesP3/Assets/wesley_temp/DashAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashAttack : MonoBehaviour
+{
+
+    public Animator dashAnim;
+    public Transform dashAnimPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("e")){
+            dashAnimPos.position.Set(-0.8f, -0.8f, 0f);
+            dashAnim.SetTrigger("Dash");
+            transform.Translate(new Vector3(2f, 2f, 0f));
+        }
+    }
+}
