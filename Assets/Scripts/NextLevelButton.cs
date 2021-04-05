3d268b82926deebb00daa948527dfa0eb6b25420
wlshen123 <WesleyLShen@gmail.com>
2021-04-05 12:30:29 -0400

    integration for alpha

---
+++ Assets/Scripts/NextLevelButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextLevelButton : MonoBehaviour
+{
+    Button nextLevelButton;
+    public string nextLevelSceneName;
+
+    void Start()
+    {
+        nextLevelButton = GetComponent<Button>();
+        nextLevelButton.onClick.AddListener(LoadNextLevel);
+    }
+
+    void LoadNextLevel(){
+        Debug.Log("loading scene: " + nextLevelSceneName);
+        SceneManager.LoadScene(nextLevelSceneName);
+    }
+}
