e21f8ec20d7cc32fb1143f8ea9ff7b68f1b63f6d
Michael Ohtake <mikado98765@gmail.com>
2021-04-05 04:51:29 -0400

    Title Scene and Screen finished

---
+++ Assets/deleteVideoPlayerWhenFinished.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class deleteVideoPlayerWhenFinished : MonoBehaviour
+{
+    private bool started_playing = false;
+    public VideoPlayer myVidPlayer;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myVidPlayer.isPlaying) {
+            started_playing = true;
+        }
+        if(started_playing && !myVidPlayer.isPlaying) {
+            canvas.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
