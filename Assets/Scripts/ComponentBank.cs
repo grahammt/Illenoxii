005aa58ad0669fefd5f1068f8274725afdce067d
wlshen123 <WesleyLShen@gmail.com>
2021-04-06 03:00:38 -0400

    added basic scene transition to tutorial and level 1

---
+++ Assets/Scripts/ComponentBank.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentBank : MonoBehaviour
+{
+    #region Singleton
+    public static ComponentBank instance;
+    void Awake(){
+        if (instance != null){
+            Destroy(this);
+        }
+        else{
+            instance = this;
+        }
+    }
+    #endregion
+
+    public Animator SceneTransitionAnimator;
+}
--- Assets/Scripts/NextLevelButton.cs
+++ Assets/Scripts/NextLevelButton.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class NextLevelButton : MonoBehaviour
 {
     Button nextLevelButton;
-    public string nextLevelSceneName;
+    public int nextLevelSceneIndex;
 
     void Start()
     {
@@ -16,7 +16,13 @@ public class NextLevelButton : MonoBehaviour
     }
 
     void LoadNextLevel(){
-        Debug.Log("loading scene: " + nextLevelSceneName);
-        SceneManager.LoadScene(nextLevelSceneName);
+        Debug.Log("button clicked");
+        StartCoroutine("LoadSceneAfterDelay", nextLevelSceneIndex);
+    }
+
+    IEnumerator LoadSceneAfterDelay(int sceneIndex){
+        ComponentBank.instance.SceneTransitionAnimator.SetTrigger("Fade");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneIndex);
     }
 }
